cf0b119bc38dd5836c1be4f6f3882e09663309ed
kishankumar <kishan.kundanjha@gmail.com>
2025-09-01 10:34:21 +0530

    Initial commit

---
+++ src/main/java/com/example/graphql_basics/GraphqlBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphql_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlBasicsApplication.class, args);
+	}
+
+}
